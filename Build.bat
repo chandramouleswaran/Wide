852e91d6a32a4cca8b406208b893af9a5e24f1d5
Chandramouleswaran <chandramouleswaran@gmail.com>
2013-02-28 23:23:51 -0600

    Update AvalonDock.dll to Nuget current version.

--- Build.bat
+++ Build.bat
@@ -1,5 +1,5 @@
 msbuild Wide.sln /t:clean
 FOR /D %%p IN ("Build\*.*") DO rmdir "%%p" /s /q
 rmdir Build /s /q
-msbuild Wide.sln /p:Configuration=Debug
+msbuild Wide.sln /p:Configuration=Release
 xcopy "Libs"  "Build\External" /E /Y /I /R
